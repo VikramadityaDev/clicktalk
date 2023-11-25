a8aeb734f39b23f2639402412b3a71f23a90b502
VIKRAMADITYA <vikramadityapratap35@gmail.com>
2023-11-25 20:23:29 +0530

    initial commit

---
+++ lib/screen/homepage.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+import '../widgets/appbar.dart';
+import '../widgets/button.dart';
+import '../widgets/inputs.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final inputController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        color: Colors.black,
+        child: Stack(
+          children: [
+            Image.asset(
+              "assets/background/background.png",
+              height: double.infinity,
+              width: double.infinity,
+              fit: BoxFit.fill,
+            ),
+            Scaffold(
+              backgroundColor: Colors.transparent,
+              body: SafeArea(
+                child: Column(
+                  children: [
+                    const AppbarDesign(),
+                    SizedBox(
+                      height: 30.h,
+                    ),
+                    Form(
+                      key: _formKey,
+                      child: Input(
+                        controller: inputController,
+                      ),
+                    ),
+                    Button(
+                      text: "Message Number",
+                      onTap: () {
+                        if (_formKey.currentState!.validate()) {
+                          if (inputController.text.isEmpty) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(
+                                  "This Field is Required!!",
+                                  style: TextStyle(
+                                      fontSize: 15.sp,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                behavior: SnackBarBehavior.floating,
+                                backgroundColor: Colors.red,
+                              ),
+                            );
+                          } else {
+                            launchUrlString(
+                              "https://wa.me/+91${inputController.text}",
+                            );
+                          }
+                        }
+                      },
+                      inputController: inputController,
+                    ),
+                  ],
+                ),
+              ),
+              bottomNavigationBar: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                  color: Color(0xff25D366)
+                  ),
+                    child: Text(
+                      '</> & Crafted with 💛 Vikramaditya',
+                      style: TextStyle(color: Colors.white, fontSize: 15),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/inputs.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class Input extends StatelessWidget {
+  const Input({
+    Key? key,
+    this.controller,
+  }) : super(key: key);
+
+  final TextEditingController? controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsetsDirectional.only(start: 16.w, end: 16.w, bottom: 50.h),
+      child: TextFormField(
+        controller: controller,
+        style: const TextStyle(
+          color: Colors.white,
+        ),
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(
+                15.r,
+              ),
+              borderSide: const BorderSide(
+                color: Color(
+                  0xff25D366,
+                ),
+              )),
+          filled: true,
+          fillColor: Colors.black,
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(
+                15.r,
+              ),
+              borderSide: const BorderSide(
+                color: Color(
+                  0xff25D366,
+                ),
+              )),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(
+                15.r,
+              ),
+              borderSide: const BorderSide(
+                color: Colors.white,
+              )),
+          labelText: "Write WhatsApp Number...",
+          labelStyle: TextStyle(
+            fontSize: 16.sp,
+            fontWeight: FontWeight.w400,
+            color: Colors.white,
+          ),
+        ),
+        onTapOutside: (event) {
+          FocusManager.instance.primaryFocus?.unfocus();
+        },
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return 'Please enter a WhatsApp number.';
+          } else if (value.length != 10 || !RegExp(r'^[0-9]+$').hasMatch(value)) {
+            return 'Please enter a valid 10-digit number.';
+          }
+          return null;
+        },
+      ),
+    );
+  }
+}
